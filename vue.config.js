88e53d3a86fb896470e41c586d8ad03e78868233
Your Name <2803137353@qq.com>
2025-07-09 16:36:33 +0800

    更新 README.md

--- vue.config.js
+++ vue.config.js
@@ -12,23 +12,14 @@ module.exports = {
       .set('_c', resolve('src/components')) // key,value自行定义，比如.set('@@', resolve('src/components'))
   },
   devServer: {
+    // 修改代理配置，使用硬编码的值而不是环境变量
     proxy: {
-      // 开发服务器代理的配置。
-      // 它会将API请求转发到后端服务器，以避免跨域问题。
-      [process.env.VUE_APP_BASE_API]: {
-        // 后端API的目标URL。
-        // 这是从 .env.development 文件中读取的。
-        target: process.env.VUE_APP_API_HOST,
-        // 对于虚拟主机站点是必需的。
-        // 它会将请求头中的'Host'更改为目标URL。
+      '/api': {
+        target: 'http://localhost:8080',
         changeOrigin: true,
-        // 代理 websocket。
         ws: true,
-        // 重写请求路径。
         pathRewrite: {
-          // 从请求路径中移除 /api 前缀。
-          // 例如，一个到 /api/users 的请求将被转发到 /users。
-          ['^' + process.env.VUE_APP_BASE_API]: ''
+          '^/api': ''
         }
       }
     }
